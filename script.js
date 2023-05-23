9b2e34286a6aa51d33b3d0ce0664b2d83dfec530
arfinzz <khanarfin2000@gmail.com>
2023-05-23 19:56:33 +0530

    Async fun implementation

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-let url='https://crudcrud.com/api/7c9943dc17594d23a70ea6b486ff1ff9/items/';
+let url = 'https://crudcrud.com/api/c71d8e98ca724d38a46734cd36e80118/items/';
 
 
-document.addEventListener('DOMContentLoaded',getItems);
+document.addEventListener('DOMContentLoaded', getItems);
 
 
-function getItems(){
-    axios.get(url).then(obj=>display(obj.data)).catch(err=>console.log(err));
-}
+async function getItems() {
 
-function addItem(e){
-    e.preventDefault();
-    let name=document.querySelector('#itemName').value;
-    let discription=document.querySelector('#itemDiscription').value;
-    let price=document.querySelector('#itemPrice').value;
-    let quantity=document.querySelector('#itemQuantity').value;
 
-    let obj={
-        "itemName":name,
-        "itemDiscription":discription,
-        "itemPrice":price,
-        "itemQuantity":quantity
+    try {
+        let obj = await axios.get(url);
+        //console.log(obj);
+        display(obj.data);
+        //return obj.data;
+    } catch (err) {
+        console.log(err);
     }
 
-    axios.post(url,obj).then(()=>{
+
+}
+
+
+
+
+
+async function addItem(e) {
+    e.preventDefault();
+    let name = document.querySelector('#itemName').value;
+    let discription = document.querySelector('#itemDiscription').value;
+    let price = document.querySelector('#itemPrice').value;
+    let quantity = document.querySelector('#itemQuantity').value;
+
+    let obj = {
+        "itemName": name,
+        "itemDiscription": discription,
+        "itemPrice": price,
+        "itemQuantity": quantity
+    }
+
+    try {
+
+
+        await axios.post(url, obj);
         let listParent = document.querySelector('.list-group');
-        let text=`<li class="list-group-item"><div id="itemID" style="display: none;">${obj._id}</div> 
+        let text = `<li class="list-group-item"><div id="itemID" style="display: none;">${obj._id}</div> 
         <div class="row align-items-center">
             <div class="col">Item Name : ${obj.itemName}</div>
             <div class="col">Discription : ${obj.itemDiscription}</div>
@@ -38,22 +56,25 @@ function addItem(e){
                
         </li>`;
 
-        listParent.innerHTML+=text;
-    }).catch(err=>console.log(err));
+
+        listParent.innerHTML += text;
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 
 
 
-function display(obj){
-   let listParent = document.querySelector('.list-group');
-   let listChild=document.querySelector('.list-group-item');
-   listChild.remove();
-   let text="";
+function display(obj) {
+    let listParent = document.querySelector('.list-group');
+    let listChild = document.querySelector('.list-group-item');
+    listChild.remove();
+    let text = "";
 
-   for(let i=0;i<obj.length;i++)
-   {
-    text+=`<li class="list-group-item"><div id="itemID" style="display: none;">${obj[i]._id}</div> 
+    for (let i = 0; i < obj.length; i++) {
+        text += `<li class="list-group-item"><div id="itemID" style="display: none;">${obj[i]._id}</div> 
     <div class="row align-items-center">
         <div class="col">Item Name : ${obj[i].itemName}</div>
         <div class="col">Discription : ${obj[i].itemDiscription}</div>
@@ -66,112 +87,153 @@ function display(obj){
     </div>
            
     </li>`;
-   }
+    }
 
-   listParent.innerHTML=text;
+    listParent.innerHTML = text;
 
 
 }
 
 
-function buyOne(e){
-    let listNode=e.parentElement.parentElement.parentElement
-    let elements=listNode.children;
-    let id=elements[0].textContent;
-    let path=url+id;
-    axios.get(path).then((obj)=>{
-        let quant=Number(obj.data.itemQuantity);
-        if(quant===1){
-            axios.delete(path).then(()=>getItems()).catch((err)=>console.log(err));
+async function buyOne(e) {
+    let listNode = e.parentElement.parentElement.parentElement
+    let elements = listNode.children;
+    let id = elements[0].textContent;
+    let path = url + id;
+
+    try {
+        let obj = await axios.get(path)
+
+        let quant = Number(obj.data.itemQuantity);
+        if (quant === 1) {
+            try {
+                await axios.delete(path);
+                await getItems();
+            } catch (err) {
+                console.log(err);
+            }
+
         }
-        else{
-            let newObj={
-            "itemName":obj.data.itemName,
-            "itemDiscription":obj.data.itemDiscription,
-            "itemPrice":obj.data.itemPrice,
-            "itemQuantity":quant-1
+        else {
+            let newObj = {
+                "itemName": obj.data.itemName,
+                "itemDiscription": obj.data.itemDiscription,
+                "itemPrice": obj.data.itemPrice,
+                "itemQuantity": quant - 1
 
             }
-            
-            axios.put(path,newObj).then(()=>getItems()).catch((err)=>console.log(err));
-            
-        }
-    }).catch((err)=>console.log(err));
-    
-   
-    
+            try {
+                await axios.put(path, newObj);
+                await getItems();
+            } catch (err) {
+                console.log(err);
+            }
 
-    
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+
+
+
+
+
 }
 
 
-function buyTwo(e){
-    let listNode=e.parentElement.parentElement.parentElement
-    let elements=listNode.children;
-    let id=elements[0].textContent;
-    let path=url+id;
-    axios.get(path).then((obj)=>{
-        let quant=Number(obj.data.itemQuantity);
-        if(quant<2){
+async function buyTwo(e) {
+    let listNode = e.parentElement.parentElement.parentElement
+    let elements = listNode.children;
+    let id = elements[0].textContent;
+    let path = url + id;
+    try {
+
+
+
+        let obj = await axios.get(path);
+        let quant = Number(obj.data.itemQuantity);
+        if (quant < 2) {
             alert("Item quantity less than desired amount");
         }
-        else if(quant==2){
-            axios.delete(path).then(()=>getItems()).catch((err)=>console.log(err));
+        else if (quant == 2) {
+            await axios.delete(path);
+            await getItems();
         }
-        else{
-            let newObj={
-            "itemName":obj.data.itemName,
-            "itemDiscription":obj.data.itemDiscription,
-            "itemPrice":obj.data.itemPrice,
-            "itemQuantity":quant-2
+        else {
+            let newObj = {
+                "itemName": obj.data.itemName,
+                "itemDiscription": obj.data.itemDiscription,
+                "itemPrice": obj.data.itemPrice,
+                "itemQuantity": quant - 2
 
             }
-            
-            axios.put(path,newObj).then(()=>getItems()).catch((err)=>console.log(err));
-            
-        }
-    }).catch((err)=>console.log(err));
-    
-   
-    
 
-    
+            await axios.put(path, newObj);
+            await getItems();
+
+        }
+
+    } catch (err) {
+        //console.log("this is my error");
+        console.log(err);
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
 
-function buyThree(e){
-    let listNode=e.parentElement.parentElement.parentElement
-    let elements=listNode.children;
-    let id=elements[0].textContent;
-    let path=url+id;
-    axios.get(path).then((obj)=>{
-        let quant=Number(obj.data.itemQuantity);
-        if(quant<3){
+async function buyThree(e) {
+    let listNode = e.parentElement.parentElement.parentElement
+    let elements = listNode.children;
+    let id = elements[0].textContent;
+    let path = url + id;
+    
+    try {
+
+
+
+        let obj = await axios.get(path);
+        let quant = Number(obj.data.itemQuantity);
+        if (quant < 3) {
             alert("Item quantity less than desired amount");
         }
-        else if(quant==3){
-            axios.delete(path).then(()=>getItems()).catch((err)=>console.log(err));
+        else if (quant == 3) {
+            await axios.delete(path);
+            await getItems();
         }
-        else{
-            let newObj={
-            "itemName":obj.data.itemName,
-            "itemDiscription":obj.data.itemDiscription,
-            "itemPrice":obj.data.itemPrice,
-            "itemQuantity":quant-3
+        else {
+            let newObj = {
+                "itemName": obj.data.itemName,
+                "itemDiscription": obj.data.itemDiscription,
+                "itemPrice": obj.data.itemPrice,
+                "itemQuantity": quant - 3
 
             }
-            
-            axios.put(path,newObj).then(()=>getItems()).catch((err)=>console.log(err));
-            
-        }
-    }).catch((err)=>console.log(err));
-    
-   
-    
 
-    
+            await axios.put(path, newObj);
+            await getItems();
+
+        }
+
+    } catch (err) {
+        //console.log("this is my error");
+        console.log(err);
+    }
+
+
+
+
+
 }
 
 
-document.querySelector('#addbtn').addEventListener('click',addItem);
+document.querySelector('#addbtn').addEventListener('click', addItem);
